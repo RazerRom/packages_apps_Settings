DirtyUnicorns/android_packages_apps_Settings

App Circle Bar Tile [2/3]
Change-Id: I4402fb871cd90adcd1b434b6d07d00d6f552cf2e

src/com/android/settings/SettingsActivity.java
@@ -115,6 +115,7 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.dirtyunicorns.dutweaks.fragments.HeadsUpSettings;
 import com.dirtyunicorns.dutweaks.fragments.NavBar;
+import com.dirtyunicorns.dutweaks.fragments.AppCircleBar;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -304,7 +305,8 @@ public class SettingsActivity extends Activity
             ApnSettings.class.getName(),
             DirtyTweaks.class.getName(),
             HeadsUpSettings.class.getName(),
-            NavBar.class.getName()
+            NavBar.class.getName(),
+            AppCircleBar.class.getName()
     };
 
 

RazerRom/packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -78,6 +78,7 @@ import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.blacklist.BlacklistSettings;
+import com.android.settings.bliss.AppCircleBar;
 import com.android.settings.bliss.HeadsUpSettings;
 import com.android.settings.bliss.NavBar;
 import com.android.settings.bluetooth.BluetoothSettings;
@@ -326,7 +327,8 @@ public class SettingsActivity extends Activity
             ExpandedDesktopPreferenceFragment.class.getName(),
             NotificationManagerSettings.class.getName(),
             LockScreenSettings.class.getName(),
-            NavBar.class.getName()
+            NavBar.class.getName(),
+            AppCircleBar.class.getName()
     };
 
 