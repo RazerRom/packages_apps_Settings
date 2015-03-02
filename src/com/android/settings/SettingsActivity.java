DirtyUnicorns/android_packages_apps_Settings

NavBar tile [2/3]
Change-Id: I684e370b813d03782d01e2abc0d28f06560a4cf0

src/com/android/settings/SettingsActivity.java
@@ -114,6 +114,7 @@ import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.dirtyunicorns.dutweaks.fragments.HeadsUpSettings;
+import com.dirtyunicorns.dutweaks.fragments.NavBar;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -302,7 +303,8 @@ public class SettingsActivity extends Activity
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
             DirtyTweaks.class.getName(),
-            HeadsUpSettings.class.getName()
+            HeadsUpSettings.class.getName(),
+            NavBar.class.getName()
     };
 
 

RazerRom/packages_apps_Settings

Conflicts:
	AndroidManifest.xml

src/com/android/settings/SettingsActivity.java
@@ -79,6 +79,7 @@ import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bliss.HeadsUpSettings;
+import com.android.settings.bliss.NavBar;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
@@ -324,7 +325,8 @@ public class SettingsActivity extends Activity
             DisplayRotation.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
             NotificationManagerSettings.class.getName(),
-            LockScreenSettings.class.getName()
+            LockScreenSettings.class.getName(),
+            NavBar.class.getName()
     };
 
 