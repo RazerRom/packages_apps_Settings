TeamBliss-LP/android_packages_apps_Settings

Settings: Add HWKeys tile.
Change-Id: Id620f114038be0b8620fbb742e84c6b7b1cfcfd3

src/com/android/settings/SettingsActivity.java
@@ -81,6 +81,7 @@ import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bliss.AppCircleBar;
 import com.android.settings.bliss.HeadsUpSettings;
 import com.android.settings.bliss.NavBar;
+import com.android.settings.ButtonSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
@@ -330,7 +331,8 @@ public class SettingsActivity extends Activity
             LockScreenSettings.class.getName(),
             NavBar.class.getName(),
             AppCircleBar.class.getName(),
-            AmbientSettings.class.getName()
+            AmbientSettings.class.getName(),
+            ButtonSettings.class.getName()
     };
 
 

RazerRom/packages_apps_Settings

Conflicts:
	AndroidManifest.xml

src/com/android/settings/SettingsActivity.java
@@ -81,6 +81,7 @@ import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.cyanogenmod.NavBar;
 import com.android.settings.bliss.AppCircleBar;
 import com.android.settings.bliss.HeadsUpSettings;
+import com.android.settings.ButtonSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
@@ -334,7 +335,8 @@ public class SettingsActivity extends Activity
             DisplayRotation.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
             NavBar.class.getName(),
-            AppCircleBar.class.getName()
+            AppCircleBar.class.getName(),
+            ButtonSettings.class.getName()
     };
 
 