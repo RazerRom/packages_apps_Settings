omnirom/android_packages_apps_Settings

Settings: use volume adjust sound setting also to volume seekbar
its pretty logical to enable/disable the seekbar adjust sound
with the same setting

Change-Id: I8cbdc74c60fad6872d6c7361975f02a73cc7bb60

src/com/android/settings/notification/VolumeSeekBarPreference.java
@@ -23,6 +23,7 @@ import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.preference.SeekBarPreference;
 import android.preference.SeekBarVolumizer;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -104,6 +105,15 @@ public class VolumeSeekBarPreference extends SeekBarPreference
                         boolean fromTouch) {
                     super.onProgressChanged(seekBar, progress, fromTouch);
                     mCallback.onStreamValueChanged(mStream, progress);
+                }
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    final boolean volumeAdjustSound = Settings.System.getInt(getContext().getContentResolver(),
+                            Settings.System.VOLUME_ADJUST_SOUND, 1) == 1;
+                    // if play adjust sound
+                    if (volumeAdjustSound) {
+                        super.onStopTrackingTouch(seekBar);
+                    }
                 }
             };
         }

RazerRom/packages_apps_Settings

Change-Id: Id79a3e83f48c31fa162cd0ba8233ad30290657c3

src/com/android/settings/notification/VolumeSeekBarPreference.java
@@ -24,6 +24,7 @@ import android.preference.PreferenceManager;
 import android.preference.SeekBarPreference;
 import android.preference.SeekBarVolumizer;
 import android.text.TextUtils;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -131,7 +132,17 @@ public class VolumeSeekBarPreference extends SeekBarPreference
         };
         final Uri sampleUri = mStream == AudioManager.STREAM_MUSIC ? getMediaVolumeUri() : null;
         if (mVolumizer == null) {
-            mVolumizer = new SeekBarVolumizer(getContext(), mStream, sampleUri, sbvc);
+            mVolumizer = new SeekBarVolumizer(getContext(), mStream, sampleUri, sbvc) {
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    final boolean volumeAdjustSound = Settings.System.getInt(getContext().getContentResolver(),
+                            Settings.System.VOLUME_ADJUST_SOUNDS_ENABLED, 1) == 1;
+                    // if play adjust sound
+                    if (volumeAdjustSound) {
+                        super.onStopTrackingTouch(seekBar);
+                    }
+                }
+            };
         }
         mVolumizer.start();
         mVolumizer.setSeekBar(mSeekBar);