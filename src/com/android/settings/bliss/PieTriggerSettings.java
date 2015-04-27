PAC-ROM/android_packages_apps_Settings

SlimPIE: Configurable sensitivity [2/2]
Change-Id: Ica4e0c7f4ed4ca6d61f5c0ccba2f1fbe5ad6b760
Signed-off-by: Lokesh Chamane <lokesh.c703@gmail.com>

src/com/android/settings/pac/pie/PieTriggerSettings.java
@@ -30,6 +30,7 @@ import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.util.Log;
 
+import com.android.settings.pac.SeekBarPreferenceCHOS;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -42,6 +43,7 @@ public class PieTriggerSettings extends SettingsPreferenceFragment
     private static final int DEFAULT_POSITION = 1 << 0;
 
     private static final String PREF_PIE_DISABLE_IME_TRIGGERS = "pie_disable_ime_triggers";
+    private static final String PIE_TRIGGER_SENSITIVITY = "pie_trigger_sensitivity";
 
     private static final String[] TRIGGER = {
         "pie_control_trigger_left",
@@ -52,6 +54,7 @@ public class PieTriggerSettings extends SettingsPreferenceFragment
 
     private CheckBoxPreference[] mTrigger = new CheckBoxPreference[4];
     private CheckBoxPreference mDisableImeTriggers;
+    private SeekBarPreferenceCHOS mSensitivity;
 
     private ContentObserver mPieTriggerObserver = new ContentObserver(new Handler()) {
         @Override
@@ -75,6 +78,11 @@ public class PieTriggerSettings extends SettingsPreferenceFragment
 
         mDisableImeTriggers = (CheckBoxPreference) findPreference(PREF_PIE_DISABLE_IME_TRIGGERS);
         mDisableImeTriggers.setOnPreferenceChangeListener(this);
+
+        mSensitivity = (SeekBarPreferenceCHOS) findPreference(PIE_TRIGGER_SENSITIVITY);
+        mSensitivity.setValue(Settings.PAC.getInt(getContentResolver(),
+                Settings.PAC.PIE_TRIGGER_SENSITIVITY, 4));
+        mSensitivity.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -85,6 +93,10 @@ public class PieTriggerSettings extends SettingsPreferenceFragment
             Settings.PAC.putInt(getContentResolver(),
                     Settings.PAC.PIE_IME_CONTROL,
                     (Boolean) newValue ? 1 : 0);
+        } else if (preference == mSensitivity) {
+            int sensitivity = ((Integer)newValue).intValue();
+            Settings.PAC.putInt(getContentResolver(),
+                    Settings.PAC.PIE_TRIGGER_SENSITIVITY, sensitivity);
         } else {
             for (int i = 0; i < mTrigger.length; i++) {
                 boolean checked = preference == mTrigger[i]

RazerRom/packages_apps_Settings

Change-Id: I7afc315bbf922f7f5ff3ba93cca38bfcd359f0ab
Signed-off-by: gamerman123x <19superguy98@gmail.com>

src/com/android/settings/bliss/PieTriggerSettings.java
@@ -28,6 +28,7 @@ import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.widget.SeekBarPreferenceCham;
 
 public class PieTriggerSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
@@ -36,6 +37,7 @@ public class PieTriggerSettings extends SettingsPreferenceFragment
     private static final int DEFAULT_POSITION = 1 << 0;
 
     private static final String PREF_PIE_DISABLE_IME_TRIGGERS = "pie_disable_ime_triggers";
+    private static final String PIE_TRIGGER_SENSITIVITY = "pie_trigger_sensitivity";
 
     private static final String[] TRIGGER = {
         "pie_control_trigger_left",
@@ -46,6 +48,7 @@ public class PieTriggerSettings extends SettingsPreferenceFragment
 
     private SwitchPreference[] mTrigger = new SwitchPreference[4];
     private SwitchPreference mDisableImeTriggers;
+    private SeekBarPreferenceCham mSensitivity;
 
     private ContentObserver mPieTriggerObserver = new ContentObserver(new Handler()) {
         @Override
@@ -69,6 +72,11 @@ public class PieTriggerSettings extends SettingsPreferenceFragment
 
         mDisableImeTriggers = (SwitchPreference) findPreference(PREF_PIE_DISABLE_IME_TRIGGERS);
         mDisableImeTriggers.setOnPreferenceChangeListener(this);
+        
+        mSensitivity = (SeekBarPreferenceCham) findPreference(PIE_TRIGGER_SENSITIVITY);
+        mSensitivity.setValue(Settings.System.getInt(getContentResolver(),
+                Settings.System.PIE_TRIGGER_SENSITIVITY, 5));
+        mSensitivity.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -78,16 +86,21 @@ public class PieTriggerSettings extends SettingsPreferenceFragment
             Settings.System.putInt(getContentResolver(),
                     Settings.System.PIE_IME_CONTROL,
                     (Boolean) newValue ? 1 : 0);
+        } else if (preference == mSensitivity) {
+            int sensitivity = ((Integer)newValue).intValue();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.PIE_TRIGGER_SENSITIVITY, sensitivity);
         } else {
             for (int i = 0; i < mTrigger.length; i++) {
                 boolean checked = preference == mTrigger[i]
                         ? (Boolean) newValue : mTrigger[i].isChecked();
                 if (checked) {
                     triggerSlots |= 1 << i;
+                }
             }
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.PIE_GRAVITY, triggerSlots);
         }
-        Settings.System.putInt(getContentResolver(),
-                Settings.System.PIE_GRAVITY, triggerSlots);
         updatePieTriggers();
         return true;
     }