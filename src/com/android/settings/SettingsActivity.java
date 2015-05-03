CyanideL/android_packages_apps_Settings

AppSideBar Tile 1/2
Single click toggle, long click settings as per usual setup

src/com/android/settings/SettingsActivity.java
@@ -135,6 +135,7 @@ import com.android.settings.cyanide.PieControl;
 import com.android.settings.cyanide.gestureanywhere.GestureAnywhereSettings;
 import com.android.settings.cyanide.NavigationSettings;
 import com.android.settings.cyanide.AppCircleBar;
+import com.android.settings.cyanide.AppSideBar;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -346,7 +347,8 @@ public class SettingsActivity extends Activity
             NavigationSettings.class.getName(),
             HeadsUpSettings.class.getName(),
             AppCircleBar.class.getName(),
-            DisplayRotation.class.getName()
+            DisplayRotation.class.getName(),
+            AppSideBar.class.getName()
     };
 
 

RazerRom/packages_apps_Settings

Change-Id: I7aa1db55abdf8f49e4bd67e516f161050a188f7c
Signed-off-by: gamerman123x <19superguy98@gmail.com>

src/com/android/settings/SettingsActivity.java
@@ -81,6 +81,7 @@ import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.cyanogenmod.NavBar;
 import com.android.settings.bliss.AppCircleBar;
+import com.android.settings.bliss.AppSidebar;
 import com.android.settings.bliss.gestureanywhere.GestureAnywhereSettings;
 import com.android.settings.bliss.HeadsUpSettings;
 import com.android.settings.ButtonSettings;
@@ -342,7 +343,8 @@ public class SettingsActivity extends Activity
             ButtonSettings.class.getName(),
             GestureAnywhereSettings.class.getName(),
             WakeLockBlocker.class.getName(),
-            AmbientSettings.class.getName()
+            AmbientSettings.class.getName(),
+            AppSidebar.class.getName()
     };
 
 